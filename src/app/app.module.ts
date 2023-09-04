383334c4a0765bbe0fbe04e06a4fb6742462535c
abdulRehamanS <abdulrehaman627@gmail.com>
2023-09-04 22:28:20 +0530

    RoutingApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+var myRoutes:Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"contact-us",component:ContactUSComponent },
+  {path:"services", component:ServicesComponent }
+];
+
+var myRoutes2 = RouterModule.forRoot(myRoutes);
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUSComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    myRoutes2
   ],
   providers: [],
   bootstrap: [AppComponent]
